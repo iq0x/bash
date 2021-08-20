3bc1d05b3a0ea01738d2e21bd2ebc03f71facf21
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-21 00:58:09 +0200

    Add files via upload

--- versandkosten.sh
+++ versandkosten.sh
@@ -3,12 +3,18 @@ echo "Versandkosten"
 echo "Gewicht: "
 read kg
 
-if [[ "$kg" -le 20 || "$kg" -ge 0 ]] ; then
-    echo "Paket kann nicht angenommen werden!!!!"
-elif [[ "$kg" -gt 10 && "$kg" -lt 20 ]]
-    echo "14 Euro"
-elif [[ "$kg" -gt 5 && "$kg" -lt 10 ]]
-    echo "10,50 Euro"
+if (( $(bc <<< "$kg > 20.0") == 1 )); then
+  echo "paket kann nicht angenommen werden"
 else
-    echo "10,50 Euro"
+  if (( $(bc <<< "$kg < 20.0 && $kg > 10.0") == 1 )); then
+    preis=14
+  else
+    if (( $(bc <<< "$kg < 10.0 && $kg > 5.0") == 1 )); then
+      preis=10.50
+    else
+      preis=7
+    fi
+  fi
 fi
+
+echo "das paket kostet" $preis "Euro"
